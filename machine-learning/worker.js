1ddfe5e63609ab8301de0c6608000ee4abbc9ce8
ryan.laiber <ryan.laiber@bagy.com.br>
2026-03-13 10:49:04 -0300

    adjust some lint errors

--- machine-learning/worker.js
+++ machine-learning/worker.js
@@ -1,20 +1,17 @@
-importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
+importScripts("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest");
 
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 
-
 self.onmessage = async ({ data }) => {
-    if (data.type !== 'predict') return
+  if (data.type !== "predict") return;
 
-    postMessage({
-        type: 'prediction',
-        x: 400,
-        y: 400,
-        score: 0
-    });
-
-
+  postMessage({
+    type: "prediction",
+    x: 400,
+    y: 400,
+    score: 0,
+  });
 };
 
-console.log('🧠 YOLOv5n Web Worker initialized');
+console.log("🧠 YOLOv5n Web Worker initialized");
